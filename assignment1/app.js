78a38d1562b9317111d4663ede1312f98a8d63de
May Thazin Thin <nanthazinmay@gmail.com>
2022-04-26 11:08:33 +0630

    push assignment 1

---
+++ assignment1/app.js
@@ -0,0 +1,18 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    newItem: '',
+    title: 'Todo App',
+    todos: ['get up', 'wash'],
+    isHidden: false
+  },
+  methods:{
+    addItem(){
+      this.todos=[...this.todos,this.newItem];
+      this.newItem='';
+    },
+    deleteItem(index){
+      this.todos.splice(index, 1);
+    }
+  }
+})
